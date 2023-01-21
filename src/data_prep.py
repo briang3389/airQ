af743028bdefd1a4f4015967c5b811bd63775dcd
Abuynits <abuynits@gmail.com>
2023-01-21 20:21:02 +0000

    i will write my will in latex

---
+++ src/data_prep.py
@@ -0,0 +1,59 @@
+from constants import *
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+
+def disp_ds_info(dl, count):
+    inp_seq = next(iter(dl))[0]
+    out_seq = next(iter(dl))[1]
+    print(" train seq:", inp_seq.shape)
+    print(" test seq", out_seq.shape)
+    for i in range(count):
+        print(" Input seq:", inp_seq[i])
+        print(" Out seq:", out_seq[i])
+
+
+class air_quality_ds(Dataset):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.data_len = len(x)
+
+    def __len__(self):
+        return self.data_len
+
+    def __getitem__(self, i):
+        train_ex = torch.from_numpy(self.x[i]).float()
+        test_ex = torch.from_numpy(self.y[i]).float()
+        return (train_ex, test_ex)
+
+
+def get_data_from(file_path):
+    # TODO: NEED TO IMPLEMENT
+    return None, None
+
+
+def get_data_loaders():
+    valid_x, valid_y = get_data_from(VALID_DATA_PATH)
+    train_x, train_y = get_data_from(TRAIN_DATA_PATH)
+
+    valid_ds = air_quality_ds(valid_x, valid_y)
+    print("validation batches:", len(valid_ds))
+
+    train_ds = air_quality_ds(train_x, train_y)
+    print("train batches:", len(train_ds))
+
+    # create dataloaders
+    valid_dl = DataLoader(valid_ds, batch_size=BATCH_SIZE, shuffle=False)
+    print(f"batches in valid_dl: {len(valid_dl)}")
+    train_dl = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+    print(f"batches in train_dl: {len(train_dl)}")
+
+    print("====Train DS====")
+    disp_ds_info(train_dl, 2)
+    print("====Validation DS====")
+    disp_ds_info(valid_dl, 4)
+
+    return train_ds, valid_ds
---
+++ src/train_model.py
@@ -0,0 +1,84 @@
+import code
+
+import torch.optim as optimizer
+from arch import *
+from constants import *
+from data_prep import *
+
+train_loss = []  # track training loss
+valid_loss = []  # track validation loss
+learning_rate = LEARNING_RATE  # the learning rate
+n_epochs = N_EPOCHS  # number of epochs
+
+# model = lstm().to(device)
+model = seq2seq().to(DEVICE)
+model_name = model.model_name
+
+print(model_name)
+loss_func = nn.MSELoss()
+optim = optimizer.Adam(model.parameters(), lr=learning_rate)
+
+num_epochs_run = 0
+
+
+def train_epoch(dl, epoch):
+    print_once = True
+    model.train(True)
+
+    epoch_train_loss = 0.
+    # loop over training batches
+    times_run = 0
+
+    for i, (x, y) in enumerate(dl):
+        optim.zero_grad()  # zero gradients
+        x = x.to(DEVICE)
+        y = y.to(DEVICE)
+
+        if model_name == "seq2seq":
+            x = x.swapaxes(0, 1)
+            y = y.swapaxes(0, 1)
+        model_out = model.forward(x)
+
+        # squeeze the tensors to account for 1 dim sizes
+        model_out = model_out.squeeze()
+        y = y.squeeze()
+
+        loss = loss_func(model_out, y)
+        epoch_train_loss += loss.item() * x.size(0)
+
+        times_run += x.size(0)
+
+        # compute the loss
+        loss.backward()
+        # step the optimizer
+        optim.step()
+
+    return epoch_train_loss / times_run
+
+
+def test_epoch(dl, epoch):
+    model.train(False)
+    epoch_test_loss = 0.
+    times_run = 0
+    # loop over testing batches
+    for i, (x, y) in enumerate(dl):
+        model_out = model(x)
+        # squeeze tensors to account for 1 dim sizes
+        model_out = model_out.squeeze()
+        y = y.squeeze()
+
+        loss = loss_func(model_out, y)
+        epoch_test_loss += loss.item() * x.size(0)
+        times_run += x.size(0)
+
+    return epoch_test_loss / times_run
+
+
+for e in range(n_epochs):
+    train_dl, valid_dl = get_data_loaders()
+    avg_train_loss = train_epoch(train_dl, e)
+    avg_valid_epoch = train_epoch(valid_dl, e)
+    num_epochs_run += 1
+    train_loss.append(avg_train_loss)
+    valid_loss.append(avg_valid_epoch)
+    print(f"epoch {e}: avg train loss: {avg_train_loss} avg val loss: {avg_valid_epoch}")

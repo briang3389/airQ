03b3fac3e60e75bda0740ef71815924cba3a756f
Brian Gan <briang3389@gmail.com>
2023-01-22 01:34:29 -0500

    latest frontend code

--- frontend/main.py
+++ frontend/main.py
@@ -10,40 +10,53 @@ import dash_daq as daq
 app = Dash(__name__) #also loads the css files in assets folder
 
 colsList = ['CO(GT)', 'PT08.S1(CO)', 'NMHC(GT)', 'C6H6(GT)', 'PT08.S2(NMHC)', 'NOx(GT)', 'PT08.S3(NOx)', 'NO2(GT)', 'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'RH', 'AH']
-numRows = 10
-df = pd.DataFrame()
-for col in colsList:
-    df[col] = [None] * numRows
-#print(df)
-mytable = dash_table.DataTable(
-    data=df.to_dict('records'),
-    columns=[{"name": i, "id": i, "hideable": True} for i in df.columns],
+
+mytableInput = dash_table.DataTable(
+    columns=[{"name": i, "id": i, "hideable": True} for i in colsList],
     editable = True,
     style_cell={
         'height': 'auto',
-        'minWidth': '80px', 'width': '80px', 'maxWidth': '80px',
+        'minWidth': '80px',
         'whiteSpace': 'normal'
     }
 )
-rowsinputbox = daq.NumericInput(
+rowsinputboxInput = daq.NumericInput(
         id='my-numeric-input-1',
         value=10,min=1,max=20
     )
 
+tmpthing = html.P();
 app.layout = html.Div(children=[
-    html.Div(children=["# of rows: ",rowsinputbox], style={'display': 'flex', 'justify-content': 'left', 'gap': '5px', 'align-items': 'center', 'margin': '10px'}),
-    html.Div(children=[mytable])
+    html.Div(children=[html.H3('Input', style={'textAlign': 'center', 'margin-bottom': '-50px'})]),
+    html.Div(children=["# of rows: ",rowsinputboxInput], style={'display': 'flex', 'justify-content': 'left', 'gap': '5px', 'align-items': 'center', 'margin': '10px'}),
+    html.Div(children=[mytableInput]),
+    tmpthing
 ])
 
 @app.callback(
-    Output(mytable, component_property='data'),
-    inputs=[Input(rowsinputbox, 'value')]
+    Output(mytableInput, component_property='data'),
+    inputs=[Input(rowsinputboxInput, 'value')]
 )
 def update_table_size(numOfRows):
     df = pd.DataFrame()
     for col in colsList:
         df[col] = [None] * numOfRows
     return df.to_dict('records')
+
+@app.callback(
+    Output(tmpthing, component_property='value'),
+    inputs=[],
+    state=[Input(mytableInput, 'data'), Input(mytableInput, 'hidden_columns')]
+)
+def dothings(tabledata, hiddencolumns):
+    df = pd.DataFrame(tabledata)
+    if (not (hiddencolumns is None)) and len(hiddencolumns) > 0:
+        df.drop(hiddencolumns, axis=1, inplace=True)
+    #for col in columnsdata:
+    #    print(col)
+        #df.drop([col['name']], axis=1, inplace=True)
+    #print(tabledata)
+    print(df)
 
 
 # Run app
